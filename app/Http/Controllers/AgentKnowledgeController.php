304efa1d18eba555358a921066a77b18f98d3562
fachrul-rachman <fachrulrachman21@gmail.com>
2025-09-24 13:43:54 +0700

    add use

--- app/Http/Controllers/AgentKnowledgeController.php
+++ app/Http/Controllers/AgentKnowledgeController.php
@@ -11,6 +11,8 @@ use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 use Symfony\Component\Process\Process;
+use Illuminate\Support\Facades\Log;
+
 
 class AgentKnowledgeController extends Controller
 {
